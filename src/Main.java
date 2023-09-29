3d7ea667cddfc30b1deaa76493035574709085bc
Nhatthach2703 <61878027+Nhatthach2703@users.noreply.github.com>
2023-09-29 13:38:31 +0700

    commit 1

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+
+import Controller.ManageEastAsiaCountries;
+
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+/**
+ *
+ * @author Xuan Vinh
+ */
+public class Main {
+    public static void main(String[] args) {
+        new ManageEastAsiaCountries().run();
+    }
+}
